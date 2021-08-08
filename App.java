a43dc72e099113bcd994ca8b331701d108a84987
Arctic <37451716+arcticbcn@users.noreply.github.com>
2021-08-08 14:27:42 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+	public static void main(String[] args) throws Exception {
+		System.out.println("C(15,5)  = " + Combinatorics.combinaciones(15, 5));
+		System.out.println("CR(5,15) = " + Combinatorics.combinacionesConRepeticion(5, 15));
+		System.out.println("P(15)    = " + Combinatorics.permutaciones(15));
+		System.out.println("V(15,5)  = " + Combinatorics.variaciones(15, 5));
+		System.out.println("VR(5,10) = " + Combinatorics.variacionesConRepeticion(5, 10));
+	}
+}
---
+++ Combinatorics.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+
+public class Combinatorics {
+
+	public static BigInteger combinaciones(int n, int k) {
+		return factorial(n).divide(factorial(k).multiply(factorial(n - k)));
+	}
+
+	public static BigInteger combinacionesConRepeticion(int n, int k) {
+		return combinaciones(n + k - 1, k);
+	}
+
+	public static BigInteger permutaciones(int n) {
+		return factorial(n);
+	}
+
+	public static BigInteger variaciones(int m, int n) {
+		return factorial(m).divide(factorial(m - n));
+	}
+
+	public static BigInteger variacionesConRepeticion(int m, int n) {
+		BigInteger bigIntM = BigInteger.valueOf(m).pow(n);
+		
+		return bigIntM;
+	}
+
+	private static BigInteger factorial(int n) {
+		BigInteger factorial = BigInteger.ONE;
+		for (int i = 2; i <= n; i++) factorial = factorial.multiply(BigInteger.valueOf(i));
+
+		return factorial;
+	}
+}
